8c4b92ae19f413905d458b288fb596557734c614
Michael Muraya <mkibabu@gmail.com>
2016-04-09 01:43:59 -0400

    First unit test and url mapping, dummy view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,14 +1,19 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
-# while functional_tests.py imported unittest and inherits from unittest.TestCase,
-# Django helpfully suggests that we us a special version of TestCase, which it comes
-# bundled with.
+from lists.views import home_page
 
-# Create your tests here.
+# Django's workflow: MVC, where views are more like controllers, and templates are
+# actually the views. When a HTTP request comes in, Django uses rules to figure out
+# which view function to call (this is called "resolving the url". The view function
+# then processes the url and returns a http response.
 
+# Here, we create a test to resolve the root of the site ("/") and return a HTTP 
+# response.
 
-# Deliberately silly etst, to test the automated test runner. The automated test
-# runner is a manage.py command, 'test'.
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page(self):
+        # Django's resolve('path') function is used to resolve urls. We'll use it
+        # to check that '/' resolves to a function called home_page
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
