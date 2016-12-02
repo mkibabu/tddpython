4e0314a3bced4c541d3da855f4f2873d63b412d0
Michael Muraya <mmuraya@lulu.com>
2016-12-02 13:21:41 -0500

    First start for styling functional tests

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -95,5 +95,21 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Eat more kale', page_text)
         self.assertNotIn('Play with the cat', page_text)
 
-        self.fail('Finish the test!')
+    
+    def test_layout_and_styling(self):
+        """Tests that the static files (here, CSS) are loading, by checking one
+        aspect of the layout
+        """
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 
+                              512,
+                              delta=5)
+
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.loation['x'] + inputbox.size['width'] / 2,
+                               512,
+                               delta = 5)
