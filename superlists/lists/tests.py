66f4d485c95f24d52ebbff41ac2aeba28eead2cc
Michael Muraya <mmuraya@lulu.com>
2016-12-02 12:40:53 -0500

    Add url for adding new item to an existing list via POST

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -60,21 +60,33 @@ class ListAndItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, second_item.text)
         self.assertEqual(second_saved_item.list, list_)
 
-class LiveViewTest(TestCase):
+class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list/')
+        list_ = List.objects.create()
+        response = self.client.get('/lists/{}/'.format(list_.id))
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list = list_)
-        Item.objects.create(text='itemey 2', list = list_)
+    def test_displays_only_items_in_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other itemey 1', list=other_list)
+        Item.objects.create(text='other itemey 2', list=other_list)
 
-        response = self.client.get('/lists/the-only-list/')
+        response = self.client.get('/lists/{}/'.format(correct_list.id))
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other itemey 1')
+        self.assertNotContains(response, 'other itemey 2')
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/{}/'.format(correct_list.id))
+        self.assertEqual(response.context['list'], correct_list)
 
 class NewListTest(TestCase):
 
@@ -90,7 +102,32 @@ class NewListTest(TestCase):
     def test_home_page_redirects_after_post(self):
         response = self.client.post(
                 '/lists/new', data = {'item_text': 'A new list item'})
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/{}/'.format(new_list.id))
 
-        self.assertRedirects(response, '/lists/the-only-list/')
+class NewItemTest(TestCase):
 
+    def test_can_save_a_POST_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        item_to_add = 'A new item to an existing list'
 
+        self.client.post(
+            '/lists/{}/add_item'.format(correct_list.id),
+            data = {'item_text': item_to_add})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, item_to_add)
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        item_to_add = 'A new item to an existing list'
+
+        response = self.client.post(
+            '/lists/{}/add_item'.format(correct_list.id),
+            data = {'item_text': item_to_add}) 
+
+        self.assertRedirects(response, '/lists/{}/'.format(correct_list.id))
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -21,5 +21,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/the-only-list/$', views.view_list, name='view_list'),
+    # since view_list uses the list id as the second argument, have the url
+    # pattern include a capture group (.+), which will match any digit(s),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
